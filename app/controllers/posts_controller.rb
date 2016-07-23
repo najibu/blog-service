8b0e537264780378673bb9ee75162fcba69a7802
Najibu Nsubuga <ns.najibu@gmail.com>
2016-07-23 17:51:28 +0100

    Added index page
    Added create action
    fixed flash message bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
+  def index
+  	@posts = Post.all
+  end
+
   def new
   	@post = Post.new
   end
+
+  def create
+  	@post = Post.new(post_params)
+
+  	respond_to do |format|
+  		if @post.save
+  			format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+	  	else
+	  		format.html { render :new }
+	  	end
+	  end
+  end
+
+  private
+  	def post_params
+  		params.require(:post).permit(:content)
+  	end
 end

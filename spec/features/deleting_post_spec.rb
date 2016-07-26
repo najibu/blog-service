ffb067ba1f792be2ed37bedd818af6d80ed5f6c6
najibu <ns.najibu@gmail.com>
2016-07-26 16:05:16 +0100

    Implemented deleting article feature

---
+++ spec/features/deleting_post_spec.rb
@@ -0,0 +1,14 @@
+feature 'Deleting a Post' do
+	before do 
+		@post = Post.create(content: "The first post")
+	end
+
+	scenario 'A user deletes a post' do 
+		visit '/'
+
+		click_link 'Delete'
+
+		expect(page).to have_content('Post deleted')
+		expect(page.current_path).to eq(root_path)
+	end
+end
